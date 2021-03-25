6bedf82e7904875c824ce3f743d30c2c0a3284f6
lib <dreak603@gmail.com>
2021-03-26 00:56:32 +0800

    feat: adding receiving update chat status event and produce with kafka

---
+++ src/handlers/chat-event.handler.ts
@@ -0,0 +1,43 @@
+import { Injectable, InternalServerErrorException, Logger } from '@nestjs/common';
+import Kafka from 'node-rdkafka';
+import { ChatEventProudcerService } from '../producers/chatevent.producer';
+import { ChatEventAggregate } from '../aggregates/chat-event.aggregate';
+import * as EChatEvt from '../aggregates/enums';
+import * as IChatEvt from '../aggregates/interfaces';
+import { config } from '../../config';
+
+@Injectable()
+export class ChatEventRoutingService {
+  private readonly ChatEventTopic = config.EVENT_STORE_SETTINGS.topics.chatEventTopic;
+
+  constructor(private readonly chatEventProudcerService: ChatEventProudcerService, private readonly chatEventAggregate: ChatEventAggregate) {}
+
+  /**
+   * @description Register socket event
+   * @public
+   * @param {string} socketMessage
+   * @returns {void}
+   */
+  public register(socketMessage: string): void {
+    if (!socketMessage) throw new InternalServerErrorException('Non socket message is being proecssed');
+    const event: IChatEvt.IEventAggregateResponse<EChatEvt.EChatEeventFromSocket, IChatEvt.IUpdateChatStatusEvt> = JSON.parse(socketMessage);
+    return this.handler(event);
+  }
+
+  /**
+   * @description Handle message delivery
+   * @private
+   * @param {IChatEvt.IEventAggregateResponse<EChatEvt.EChatEeventFromSocket, IChatEvt.IUpdateChatStatusEvt>} event
+   * @returns {void}
+   */
+  private handler(event: IChatEvt.IEventAggregateResponse<EChatEvt.EChatEeventFromSocket, IChatEvt.IUpdateChatStatusEvt>) {
+    switch (event.type) {
+      case EChatEvt.EChatEeventFromSocket.UPDATEREADSTATUS:
+        const readEvt = this.chatEventAggregate.updateReadStatusEvent(event.data);
+        return this.chatEventProudcerService.produce<IChatEvt.IEventAggregateResponse<EChatEvt.EChatEeventFromSocket.UPDATEREADSTATUS, IChatEvt.IUpdateChatStatusEvt>>(this.ChatEventTopic, readEvt, readEvt.id);
+      case EChatEvt.EChatEeventFromSocket.UPDATESENDSTATUS:
+        const sendEvt = this.chatEventAggregate.updateSendStatusEvent(event.data);
+        return this.chatEventProudcerService.produce<IChatEvt.IEventAggregateResponse<EChatEvt.EChatEeventFromSocket.UPDATESENDSTATUS, IChatEvt.IUpdateChatStatusEvt>>(this.ChatEventTopic, sendEvt, sendEvt.id);
+    }
+  }
+}
